086920f8c9ead513f48a27c39454f7d3b56b4a20
imadldn <imad.kourde.pro@gmail.com>
2023-05-01 02:13:55 +0100

    First commit

---
+++ dashboard_ts/src/App.tsx
@@ -0,0 +1,29 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { Box, CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./pages/navbar";
+import Dashboard from "./pages/dashboard"
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []); //The [] arg is there in order to make sure useMemo does this only once.
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard/>} />
+              <Route path="/returnoninvestment" element={<div>Return On Investment Page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ dashboard_ts/src/data/api.ts
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { GetKpisResponse } from "./type";
+
+export const api = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:1337" }),
+    reducerPath: "main",
+    tagTypes: ["Kpis"],
+    endpoints: (build) =>({
+        getKpis: build.query<Array<GetKpisResponse>, void>({
+            query: () => "kpi/kpis/",
+            providesTags: ["Kpis"]
+        })
+    })
+})
+
+export const { useGetKpisQuery } = api;
